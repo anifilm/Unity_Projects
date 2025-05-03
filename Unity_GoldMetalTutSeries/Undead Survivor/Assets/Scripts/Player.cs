ec5983db639fdce1dd6316a29be8fbd77da79c8f
anifilm <anifilm02@gmail.com>
2025-05-03 20:03:37 +0900

    study unity 2025-05-03

--- Unity_GoldMetalTutSeries/Undead Survivor/Assets/Scripts/Player.cs
+++ Unity_GoldMetalTutSeries/Undead Survivor/Assets/Scripts/Player.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public float speed;
+
     Rigidbody2D rigid;
+    SpriteRenderer sprite;
+    Animator animator;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        inputVec.x = Input.GetAxis("Horizontal");
-        inputVec.y = Input.GetAxis("Vertical");
+        // 인풋 시스템 변경으로 주석 처리
+        //inputVec.x = Input.GetAxis("Horizontal");
+        //inputVec.y = Input.GetAxis("Vertical");
     }
 
     void FixedUpdate()
@@ -26,7 +33,22 @@ public class Player : MonoBehaviour
         // 2. 속도 제어
         //rigid.velocity = inputVec;
         // 3. 위치 이동
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        animator.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x > 0)
+            sprite.flipX = false;
+        else if (inputVec.x < 0)
+            sprite.flipX = true;
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
     }
 }
