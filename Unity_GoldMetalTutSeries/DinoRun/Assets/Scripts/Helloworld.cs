1bb87bc918a527e941cd2a5030ed028f4e4cf65d
anifilm <anifilm02@gmail.com>
2025-05-12 09:34:49 +0900

    init files

---
+++ Unity_GoldMetalTutSeries/DinoRun/Assets/Scripts/Helloworld.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
