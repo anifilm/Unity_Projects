52168613b9426e2e483dfa5ad08ee6e1896304c2
anifilm <anifilm02@gmail.com>
2025-06-14 00:13:45 +0900

    fixed unity version

---
+++ Zigurous_Tutorials/Pacman/Assets/Scripts/Hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Scripts
+{
+    public class Hello
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
